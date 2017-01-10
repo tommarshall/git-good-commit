42a8e6669cb75177ad6f6d1cae18cb74b45a9a1b
Tom Marshall <tommarshall7@gmail.com>
2017-01-10 08:42:13 +0000

    Add adjust verbs to imperative blacklist

--- hook.sh
+++ hook.sh
@@ -165,6 +165,7 @@ validate_commit_message() {
 
   IMPERATIVE_MOOD_BLACKLIST=(
     added          adds          adding
+    adjusted       adjusts       adjusting
     amended        amends        amending
     avoided        avoids        avoiding
     bumped         bumps         bumping
